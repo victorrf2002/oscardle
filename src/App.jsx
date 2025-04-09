b8aba021f69b3fe1380d621a936d3dd20e156338
victorrf <victorrf2002@gmail.com>
2025-04-09 18:48:34 -0400

    Writing basic header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 
 import './App.css'
 
-function GameBoard() {
-  return (
-    <h1></h1>
-  );
+// Component for guess input
+function Guess() {
+
 }
 
 function Header() {
   return (
-    <header>
+    <header class="flex flex-row justify-between border-b-2 pb-2">
 
       {/* this is just a placeholder CHANGE AFTER */}
       <p id='date'>APRIL 9, 2025</p> 
@@ -25,7 +24,7 @@ function App() {
     <div>
       <Header/>
       <h1>OSCARDLE</h1>
-      <GameBoard />
+      <Guess />
     </div>
     
   );
